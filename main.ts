ec544e7a89144301f2482e62e4c23ad23b70c031
julienhogert <36716147+julienhogert@users.noreply.github.com>
2021-04-17 21:02:04 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,49 @@
-basic.forever(function () {
+function faire_demi_tour () {
 	
+}
+function avancer_en_ligne () {
+    basic.showArrow(ArrowNames.North)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, vitesse)
+}
+function suivre_la_ligne () {
+	
+}
+function reculer () {
+    basic.showArrow(ArrowNames.South)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, vitesse)
+}
+function avancer () {
+    basic.showArrow(ArrowNames.North)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, vitesse)
+}
+function tourner (direction: number) {
+    if (direction < 0) {
+        basic.showArrow(ArrowNames.East)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesse + direction)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vitesse - direction)
+    } else {
+        basic.showArrow(ArrowNames.West)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesse - direction)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vitesse + direction)
+    }
+}
+function stopper () {
+    maqueen.motorStop(maqueen.Motors.All)
+}
+function eviter_les_obstacles () {
+    stopper()
+    if (maqueen.Ultrasonic(null) < 15) {
+        basic.showIcon(IconNames.No)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesse_demi_tour)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, vitesse_demi_tour)
+    }
+}
+let vitesse_demi_tour = 0
+let vitesse = 0
+vitesse = 130
+let direction = 30
+vitesse_demi_tour = 60
+basic.forever(function () {
+    avancer()
+    eviter_les_obstacles()
 })
