e04b95946d4c3f06f4c076015b9f9f1b53ffbd8c
julienhogert <36716147+julienhogert@users.noreply.github.com>
2021-04-18 22:39:00 +0200

    Update pxt.json, counter.ts

---
+++ counter.ts
@@ -0,0 +1,9 @@
+namespace Counters {
+    /**
+    * Computes the famous Fibonacci number sequence!
+    */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    }
+}
