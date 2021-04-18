67011b42ec128e227b382f0fde67eafdacfdb0c2
julienhogert <36716147+julienhogert@users.noreply.github.com>
2021-04-18 23:53:27 +0200

    Update main.blocks, main.ts, counter.ts

--- counter.ts
+++ counter.ts
@@ -1,9 +1,26 @@
+//% color="#AA278D"
 namespace Counters {
-    /**
-    * Computes the famous Fibonacci number sequence!
-    */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
+    //%
+    export class counter { 
+         millis:number;
+         constructor() {
+            this.millis = control.millis();
+         }
+        
+    //% block="$this(counter) count until $interval and restart"
+    //% handlerStatement
+    public onEventAsStatement(interval: number, handler: () => void) {
+        if(this.millis + interval*1000 < control.millis()){
+             this.millis = control.millis();
+             handler();
+        }
+    }
+
+    }
+
+    //% block="create counters"
+    //% blockSetVariable=counter
+    export function createCounters(): counter {
+        return new counter();
     }
 }
