32fa8f634a2bae74ea0f61dd0617c59301cdb468
julienhogert <36716147+julienhogert@users.noreply.github.com>
2021-04-19 11:52:31 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function faire_demi_tour() {
+function faire_demi_tour () {
     if (Math.randomBoolean()) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesse_demi_tour)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, vitesse_demi_tour)
@@ -6,33 +6,25 @@ function faire_demi_tour() {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, vitesse_demi_tour)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vitesse_demi_tour)
     }
-    
     basic.showIcon(IconNames.Sad)
 }
-
-function avancer_en_ligne() {
+function avancer_en_ligne () {
     basic.showArrow(ArrowNames.North)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, vitesse)
 }
-
-function toggle_on_off() {
-    
+function toggle_on_off () {
     if (playstop) {
         playstop = 0
     } else {
         playstop = 1
     }
-    
 }
-
-function couleur_chassis() {
-    
+function couleur_chassis () {
     strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
     strip.shift(2)
     strip.showColor(neopixel.colors(NeoPixelColors.Orange))
 }
-
-function suivre_ligne_2() {
+function suivre_ligne_2 () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
@@ -42,7 +34,6 @@ function suivre_ligne_2() {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
             maqueen.motorStop(maqueen.Motors.M1)
         }
-        
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         maqueen.motorStop(maqueen.Motors.M2)
@@ -50,43 +41,32 @@ function suivre_ligne_2() {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
             maqueen.motorStop(maqueen.Motors.M2)
         }
-        
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         } else {
             maqueen.motorStop(maqueen.Motors.M2)
         }
-        
     }
-    
 }
-
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     dessiner()
 })
-function reculer() {
+function reculer () {
     basic.showArrow(ArrowNames.South)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, vitesse)
 }
-
-function compteur_inc() {
-    
+function compteur_inc () {
     compteur = Math.floor(control.millis() / 1000)
-    serial.writeLine("" + ("" + compteur))
+    serial.writeLine("" + compteur)
 }
-
-function compteur_dessiner() {
-    
+function compteur_dessiner () {
     if (millis_servo + servo_interval < control.millis()) {
         millis_servo = control.millis()
         servo_interval = 1000 * randint(0, 5)
         dessiner()
     }
-    
 }
-
-function suivre_une_ligne() {
-    
+function suivre_une_ligne () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
         derniere_direction = -1
         basic.showArrow(ArrowNames.East)
@@ -94,18 +74,15 @@ function suivre_une_ligne() {
         derniere_direction = 1
         basic.showArrow(ArrowNames.West)
     }
-    
 }
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     toggle_on_off()
 })
-function avancer() {
+function avancer () {
     basic.showArrow(ArrowNames.North)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, vitesse)
 }
-
-function tourner(direction: number) {
+function tourner (direction: number) {
     if (direction < 0) {
         basic.showArrow(ArrowNames.East)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesse + direction)
@@ -115,35 +92,27 @@ function tourner(direction: number) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesse - direction)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vitesse + direction)
     }
-    
 }
-
-function stopper() {
+function stopper () {
     maqueen.motorStop(maqueen.Motors.All)
     basic.showIcon(IconNames.No)
 }
-
-function eviter_les_obstacles() {
-    
+function eviter_les_obstacles () {
     distance_sonar = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (distance_sonar > 2 && distance_sonar < distance_detection) {
         soundExpression.giggle.play()
         faire_demi_tour()
         while (maqueen.Ultrasonic(PingUnit.Centimeters) > 2 && maqueen.Ultrasonic(PingUnit.Centimeters) < distance_detection) {
-            
+        	
         }
         if (playstop == 1) {
             avancer()
         } else {
             stopper()
         }
-        
     }
-    
 }
-
-function dessiner() {
-    
+function dessiner () {
     if (servo_dessin == true) {
         maqueen.servoRun(maqueen.Servos.S2, servo_angle_repos)
         basic.showIcon(IconNames.Sword)
@@ -153,14 +122,12 @@ function dessiner() {
         basic.showIcon(IconNames.House)
         servo_dessin = true
     }
-    
 }
-
 let distance_sonar = 0
 let derniere_direction = 0
 let servo_interval = 0
 let millis_servo = 0
-let strip : neopixel.Strip = null
+let strip: neopixel.Strip = null
 let playstop = 0
 let compteur = 0
 let servo_dessin = false
@@ -178,6 +145,6 @@ servo_angle_dessin = 80
 servo_angle_repos = 120
 servo_dessin = false
 compteur = 0
-basic.forever(function on_forever() {
+basic.forever(function () {
     compteur_inc()
 })
